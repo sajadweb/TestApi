99253f112d6643b3d7c34235ea027c39773921d8
sajjadweb <daram3118@gmail.com>
2020-05-26 11:24:53 +0430

    docker

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const debug = require('debug')('myapp:index');
 // make bluebird default Promise
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 mongoose.Promise = Promise;
-const mongoUri = config.mongo.host;
+const mongoUri = 'mongodb://192.18.2.69/auth';// config.mongo.host;
+console.log('db', mongoUri);
 mongoose.connect(mongoUri, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
